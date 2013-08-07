305897714f1af5b317a81ec533dc00ea42df79ad
=sam123456789 <chamaths.10@cse.mrt.ac.lk>
2013-08-07 12:50:11 +0530

    insert file
    to stable the system

---
+++ Node.java
@@ -0,0 +1,31 @@
+
+
+
+public class Node {
+
+  int b,c,d,e,f,g,h,fr,dsfd,dsfddfdf,dss;
+    private char character;
+    private int state;
+    private boolean acceptedState;
+    int a;
+    public Node(char character, int state, boolean acceptedState) {
+        this.character = character;
+        this.state = state;
+        this.acceptedState = acceptedState;
+    }
+
+    public char getCharacter() {
+        return character;
+    }
+
+    public int getState() {
+        return state;
+    }
+
+    public boolean isAcceptedState() {
+        return acceptedState;
+    }
+
+    
+
+}
